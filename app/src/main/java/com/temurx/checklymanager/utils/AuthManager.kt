58a312066d7847504c2d43ca542c4b4d2abbcbe3
Temur <temurxaydarov1@gmail.com>
2026-05-06 19:22:59 +0300

    chore(manager): remove unused AuthManager.register

--- app/src/main/java/com/temurx/checklymanager/utils/AuthManager.kt
+++ app/src/main/java/com/temurx/checklymanager/utils/AuthManager.kt
@@ -16,17 +16,6 @@ object AuthManager {
             }
     }
 
-    fun register(email: String, password: String, onResult: (Boolean, String?) -> Unit) {
-        auth.createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    onResult(true, null)
-                } else {
-                    onResult(false, task.exception?.message)
-                }
-            }
-    }
-
     fun logout() {
         auth.signOut()
     }

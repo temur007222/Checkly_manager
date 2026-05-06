d148102f6961fbb106ec570ffe5f04b512d8bb35
Temur <temurxaydarov1@gmail.com>
2026-05-06 21:48:05 +0300

    feat(manager): redesign bottom nav (Team / Stats / Profile) + edge-to-edge insets

--- app/src/main/java/com/temurx/checklymanager/MainActivity.kt
+++ app/src/main/java/com/temurx/checklymanager/MainActivity.kt
@@ -4,8 +4,9 @@ import android.os.Bundle
 import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.firebase.FirebaseApp
@@ -25,31 +26,37 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Get NavController from NavHostFragment
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { _, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            binding.navHostFragment.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
+            binding.bottomNav.setPadding(0, 0, 0, systemBars.bottom)
+            insets
+        }
+
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
 
-        // Connect BottomNavigationView with NavController
         binding.bottomNav.setupWithNavController(navController)
 
-        // Listen for destination changes to hide/show BottomNavigationView
-        // and update selected menu item
         navController.addOnDestinationChangedListener { _, destination, _ ->
+            // Hide bottom nav on login screen
+            binding.bottomNav.visibility =
+                if (destination.id == R.id.loginScreen) View.GONE else View.VISIBLE
 
-            // Hide bottom nav on login
-            binding.bottomNav.visibility = if (destination.id == R.id.loginScreen) View.GONE else View.VISIBLE
-
-            // Update BottomNavigationView selection
+            // Sync top-level destination selection
             when (destination.id) {
-                R.id.dashboardFragment -> binding.bottomNav.menu.findItem(R.id.dashboardFragment).isChecked = true
+                R.id.dashboardFragment ->
+                    binding.bottomNav.menu.findItem(R.id.dashboardFragment).isChecked = true
                 R.id.homeFragment,
                 R.id.staffDetailFragment,
                 R.id.addTaskFragment,
-                R.id.taskDetailFragment -> binding.bottomNav.menu.findItem(R.id.homeFragment).isChecked = true
-                R.id.profileFragment -> binding.bottomNav.menu.findItem(R.id.profileFragment).isChecked = true
+                R.id.taskDetailFragment,
+                R.id.addStaffFragment ->
+                    binding.bottomNav.menu.findItem(R.id.homeFragment).isChecked = true
+                R.id.profileFragment ->
+                    binding.bottomNav.menu.findItem(R.id.profileFragment).isChecked = true
             }
         }
-
     }
 }
